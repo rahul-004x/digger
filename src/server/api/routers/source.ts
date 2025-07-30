112794e267d7c81322d5932507d4744ea6016141
rahul <rahulxyz123u@gmail.com>
2025-07-30 19:56:06 +0530

    feat(llm): expose context in LLM response

--- src/server/api/routers/source.ts
+++ src/server/api/routers/source.ts
@@ -12,7 +12,7 @@ const tavilyClient = tavily({
 
 const openRouterClient = new openAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.OPENROUTER_API_KEY
+  apiKey: process.env.OPENROUTER_API_KEY,
 });
 
 export const sourceRouter = createTRPCRouter({
@@ -70,6 +70,29 @@ export const sourceRouter = createTRPCRouter({
           }
         }),
       );
+      const mainAnswerPrompt = `
+  Given a user question and some context, please write a clean, concise and accurate answer to the question based on the context. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context when crafting your answer.
+
+  Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
+
+
+  Remember, don't blindly repeat the contexts verbatim and don't tell the user how you used the citations – just respond with the answer. It is very important for my career that you follow these instructions. Here is the user question:
+    `;
+      const combinedContext = context.map((item) => item.context).join("\n\n");
+      const completions = await openRouterClient.chat.completions.create({
+        model: "moonshotai/kimi-k2:free",
+        messages: [
+          {
+            role: "system",
+            content: `${mainAnswerPrompt}`,
+          },
+          {
+            role: "user",
+            content: `Here is the extracted context from web:\n\n${combinedContext}`,
+          },
+        ],
+      });
+      console.log(completions.choices[0]?.message);
       return context;
     }),
 });
