3f95d57d0a6111606c50114740d2d14ba758d9c0
rahul <rahulxyz123u@gmail.com>
2025-09-13 12:01:22 +0530

    feat: style citatation

--- src/components/app/citations/CitationNumber.tsx
+++ src/components/app/citations/CitationNumber.tsx
@@ -1,5 +1,5 @@
 export const CitationNumber = ({ num }: { num: number }) => {
-  return <span className="mx-[1px] bg-gray-200 inline-block rounded px-1">
+  return <span className="mx-[1px] bg-gray-200 inline-block align-text-bottom text-xs rounded px-1 tabular-nums cursor-pointer">
     {num}
   </span>;
 };
